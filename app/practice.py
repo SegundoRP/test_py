818c237bcc6f9ff5633bb09f7bd29fbc3edf5a8a
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-02-02 00:59:08 -0500

    feat: :sparkles: add operations in sets

--- app/practice.py
+++ app/practice.py
@@ -10,7 +10,12 @@ print(lista[1])
 lista.append(4)
 print(lista)
 
-
+# conjunto
+conjunto1 = {1, 2, 3, 4, 2, 3}
+conjunto2 = {0, 2, 4, 2, 9}
+# cant repeat members
+print(conjunto1 | conjunto2) # union
+print(conjunto1 & conjunto2) # intersection
 
 # diccionario
 diccionario = {'clave1': 1, 'clave2': 2}
