eb11ac173909d9eba67c74bd4cc83e0487acd1e6
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-02-02 00:58:19 -0500

    feat: :sparkles: some operations with datatypes

---
+++ app/practice.py
@@ -0,0 +1,19 @@
+# tupla
+tupla = (1, 2, 3)
+# inmutable
+print(tupla[0])
+print(tupla)
+
+# lista
+lista = [1, 2, 3]
+print(lista[1])
+lista.append(4)
+print(lista)
+
+
+
+# diccionario
+diccionario = {'clave1': 1, 'clave2': 2}
+print(diccionario['clave1'])
+diccionario.pop('clave1')
+print(diccionario)
