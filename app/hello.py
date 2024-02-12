65ffa8cb82f5ea9837d0aa38617c740d4417a2a4
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-02-12 00:33:10 -0500

    feat: :sparkles: add basic validation on run

--- app/hello.py
+++ app/hello.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 # represents flask app
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run()
+
 # represents principal route
 @app.route('/')
 @app.route('/index')
